145f080b077b9528451fabbe8390ffb7a3302c47
ZAlex1007 <zambaalexandru1@gmail.com>
2021-04-26 12:13:39 +0300

    Added information

--- js/chart.js
+++ js/chart.js
@@ -9,6 +9,13 @@ let labels=['2015', '2016', '2017', '2018', '2019', '2020'];
 let values=[7.4,8.6,10.2,11.9,13.7,15.5];
 
 Chart.defaults.color="#FFF";
+Chart.defaults.borderColor	= "#a18868"
+let borderWidth=2;
+if(window.innerWidth>1600){
+     Chart.defaults.font.size=22;
+     borderWidth=5;
+}
+else Chart.defaults.font.size=14; 
 
 let data={
     labels: labels,
@@ -16,15 +23,15 @@ let data={
         label: '% din comert realizat electronic',
         data: values,
         backgroundColor: [
-            'rgba(255, 99, 132, 1)'
+            '#ff5517'
         ],
         borderColor: [
-            'rgba(255, 99, 132, 1)'
+            '#ff5517'
         ],
         color: '#FFF',
         scaleFontColor: "#FFFFFF",
 
-        borderWidth: 1,
+        borderWidth: borderWidth,
         pointRadius: 5,
         pointHitRadius: 7,
     
@@ -39,10 +46,16 @@ let chart=new Chart(ctx, {
 
     options: {
         color: '#FFF',
-        // responsive:true,
+        responsive:true,
         scaleFontColor: "#FFFFFF",
         scales: {
             y: {
+                display: true,
+
+                gridLines: {
+                    display: true,
+                    color: "#FFFFFF"
+                },
                 ticks: {
                     min: 0,
                     max: 100,
